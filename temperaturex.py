1c792f9677011d905d0fe8bed9999f638179e33a
codewith-briankikuvi <brian.daniel@strathmore.edu>
2025-03-04 11:42:57 +0300

    control flows

---
+++ temperaturex.py
@@ -0,0 +1,15 @@
+
+# temperature = 30
+
+# if temperature > 30:
+#     print("Its a hot day!")
+# else:
+#     print("It's a cool day!")
+
+def temperature(t):
+    if t > 30:
+        print("It's a hot day!")
+    else:
+        print("It's a cool day!") 
+    
+temperature(5)
