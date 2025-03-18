3c6e2ecd20bf3e398bbf85873cf599fb12688e08
codewith-briankikuvi <brian.daniel@strathmore.edu>
2025-03-18 11:36:26 +0300

    1 and True, 0 and False printing in a set

--- Python/Week 2/sets.py
+++ Python/Week 2/sets.py
@@ -1,6 +1,6 @@
 students_ids= {'001', '002', '003', '004', '005'}
 # sets ignore boolean data types.
-students_names = {'Ace', 'Tyson', 'Ford', 'Maverick', False, 45, 78.9}
+students_names = {0, 56,'Ace', 'Tyson', 'Ford', 'Maverick', 45, 78.9}
 print(students_ids)
 print(students_names)
 # sets do not allow duplicates
