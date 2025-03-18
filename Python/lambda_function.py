826be6b96b2a88c052bad41cb87d4594daec6780
codewith-briankikuvi <brian.daniel@strathmore.edu>
2025-03-18 10:54:19 +0300

    organized files

--- Python/lambda_function.py
+++
@@ -1,6 +0,0 @@
-# Lambda function to add two numbers
-add_numbers = lambda x, y: x + y
-
-# Using the lambda function
-result = add_numbers(5, 10)
-print("Sum:", result)
--- Python/list_comprehesion.py
+++
@@ -1,4 +0,0 @@
-# Generate a list of squares for numbers 1 to 10
-squares = [x**2 for x in range(1, 11)]
-
-print("List of squares:", squares)
