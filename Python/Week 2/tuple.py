027274e666bdf65d980f1163e648a00e1697741d
codewith-briankikuvi <brian.daniel@strathmore.edu>
2025-03-18 10:55:48 +0300

    tuples and lists

---
+++ Python/Week 2/tuple.py
@@ -0,0 +1,9 @@
+# tuples use normal brackets and they're immutable
+# tuples are faster than lists
+# tuples are used when you want to store multiple values in a single variable
+ 
+students = ('Brian', 'John', 'Jane', 'Alice')
+print(students)
+print(students[0]) #Brian
+print(students[-2]) #Jane
+
