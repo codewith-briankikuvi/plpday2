77db4ba237521cf6580de214c2f4bc537a657732
codewith-briankikuvi <brian.daniel@strathmore.edu>
2025-03-25 11:03:46 +0300

    if else statements

---
+++ Python/Week 3/ifelif.py
@@ -0,0 +1,10 @@
+temperature = 21
+
+if temperature > 40:
+    print("It's hot")
+elif temperature > 20:
+    print("It's warm")
+elif temperature > 10:
+    print("It's cool")
+else:
+    print("It's cold")
